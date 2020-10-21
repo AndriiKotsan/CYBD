a2ffa062cb17cd71c11919eb2a3111b930850c65
AndriiKotsan <64476604+AndriiKotsan@users.noreply.github.com>
2020-10-21 21:05:25 +0300

    Add files via upload

---
+++ Laboratorna3.sql
@@ -0,0 +1,11 @@
+﻿ALTER TABLE [dbo].[Спортсмен] 
+ADD Історія CHAR(25);
+
+ALTER TABLE [dbo].[Спортсмен]
+DROP COLUMN Історія;
+
+ALTER TABLE [dbo].[Спортсмен]
+ALTER COLUMN [Ім'я] CHAR(40) NOT NULL;
+
+ALTER TABLE [dbo].[Спортсмен]
+ADD CHECK ([Телефонний номер] > 009999999);
