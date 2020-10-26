5c628765faefd71c185a208797b0f136180d02cf
AndriiKotsan <64476604+AndriiKotsan@users.noreply.github.com>
2020-10-26 16:57:44 +0200

    Add files via upload

---
+++ Laboratorna5.sql
@@ -0,0 +1,20 @@
+﻿SELECT Прізвище + ' ' +[Ім'я] AS ПІБ
+FROM Спортсмен
+UNION SELECT Прізвище + ' ' +[Ім'я] AS ПІБ 
+FROM Суддя
+
+
+SELECT [Код спортсмена] as Перетин
+FROM Спортсмен
+INTERSECT
+SELECT  [Код судді]
+FROM Суддя
+
+
+SELECT [Код спортсмена] FROM Спортсмен
+EXCEPT
+SELECT [Код судді] FROM Суддя;
+
+
+SELECT Назва,Населення FROM [Країна-господар]
+CROSS JOIN Олімпіада
