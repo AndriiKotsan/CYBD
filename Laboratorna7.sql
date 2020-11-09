e28f29508b480855a68443c48162b42cb84884de
AndriiKotsan <64476604+AndriiKotsan@users.noreply.github.com>
2020-11-09 21:37:11 +0200

    Add files via upload

---
+++ Laboratorna7.sql
@@ -0,0 +1,68 @@
+﻿ SELECT*
+FROM dbo.Спортсмен
+WHERE Прізвище 
+LIKE 'А%'
+
+
+SELECT*
+FROM dbo.Спортсмен
+WHERE Прізвище 
+BETWEEN 'А%' 
+AND 'Д%'
+
+
+SELECT*
+FROM dbo.Спортсмен
+WHERE [По-батькові] IS NULL
+
+
+SELECT*
+FROM dbo.Спортсмен
+WHERE [По-батькові] IS NOT NULL
+
+ 
+ SELECT*
+FROM dbo.Спортсмен
+WHERE [Спортивне звання] 
+IN ('МС','КМС')
+
+
+SELECT*
+FROM dbo.Спортсмен
+WHERE [Спортивне звання] 
+NOT IN ('МС','КМС')
+
+ 
+ SELECT*
+FROM dbo.Спортсмен
+WHERE [Телефонний номер] > ALL(
+SELECT [Телефонний номер]
+FROM dbo.Суддя
+WHERE [Суддівське звання]='Заслужений суддя') 
+
+ 
+ SELECT*
+FROM dbo.Спортсмен
+WHERE [Телефонний номер] > ANY(
+SELECT [Телефонний номер]
+FROM dbo.Суддя
+WHERE [Суддівське звання]='Заслужений суддя')
+
+ 
+ SELECT*
+FROM dbo.Спортсмен
+WHERE [Телефонний номер] > SOME(
+SELECT [Телефонний номер]
+FROM dbo.Суддя
+WHERE Стать='Ч'
+AND [Телефонний номер]> 300000000) 
+
+ 
+ 
+ SELECT [Код спортсмена] FROM Спортсмен
+      AS Код
+      WHERE EXISTS (
+	  SELECT
+      [Код спортсмена] FROM Спортсмен
+      WHERE Прізвище='Кортез'
+      AND [Код спортсмена]=Код.[Код спортсмена])	
